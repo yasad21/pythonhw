df20d28b9302efd8740af5f22ee5e0bb38913852
yasad21 <144398591+yasad21@users.noreply.github.com>
2023-12-05 21:21:23 -0800

    Update print_caps.py

--- print_caps.py
+++ print_caps.py
@@ -1,6 +1,12 @@
-def allcaps(func):
-    def wrapper():
-        result = func()
-        return result.upper()
-
-    return wrapper
+def allcaps (f):
+    def inner():
+        val = f()
+        if(isinstance(val,list)):
+            for i in range(len(val)):
+                if(isinstance(val[i],str)):
+                    val[i] = val[i].upper()
+            return val
+        if(isinstance(val,str)):
+           return val.upper()
+   
+    return inner
