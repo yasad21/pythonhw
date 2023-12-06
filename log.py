da55365e19a1e3ab783a1ff3828cb16b2704c2b6
yasad21 <144398591+yasad21@users.noreply.github.com>
2023-12-05 21:19:29 -0800

    Update log.py

--- log.py
+++ log.py
@@ -1,9 +1,7 @@
 import time
 
-def timestamp(func):
-    def wrapper(*argument, **keywordargument):
-        current_time = time.ctime()
-        print(current_time)
-        func(*argument, **keywordargument)
-
-    return wrapper
+def timestamp(f):
+    def inner():
+        print(time.ctime())
+        f()
+    return inner
